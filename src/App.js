47098b62958389c56ed3a5d5d6b09623c9ba8b22
mirzavic4869 <avicenamirzam@gmail.com>
2022-09-20 13:08:52 +0700

    change function prev to seharimakan

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
 	const [sehariMakan, setSehariMakan] = useState(0);
+	const [namaMakanan, setNamaMakanan] = useState("");
+
+	useEffect(() => {
+		if (sehariMakan > 2) {
+			setNamaMakanan("Nasgor");
+		} else {
+			setNamaMakanan("sate");
+		}
+	}, [sehariMakan]);
+
 	return (
 		<div>
 			<div>
+				<h2>Saya Makan: {namaMakanan}</h2>
 				<h1>Saya sehari makan {sehariMakan} kali</h1>
-				<button onClick={() => setSehariMakan((prev) => prev + 1)}>tambah</button>
+				<button onClick={() => setSehariMakan(sehariMakan + 1)}>tambah</button>
 				{sehariMakan === 0 ? <button disabled></button> : <button onClick={() => setSehariMakan((prev) => prev - 1)}>kurang</button>}
 			</div>
 		</div>
